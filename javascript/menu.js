4b38f10b2b762b6c23d0d67b6ba91747387551d3
dennishvf <dennisk38@nycstudents.net>
2024-01-16 08:43:14 -0500

    mn m m,

---
+++ javascript/menu.js
@@ -0,0 +1,38 @@
+const Countries = [
+{
+    Name: "Ukraine",
+    Image: "https://www.worldatlas.com/r/w1200/img/flag/ua-flag.jpg",
+    Continent: "Europe"
+},
+{
+    Name: "USA",
+    Image: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png",
+    Continent: "North America"
+},
+{
+    Name: "Germany",
+    Image: "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1200px-Flag_of_Germany.svg.png",
+    Continent: "Europe",
+},
+{
+    Name: "Canada",
+    Image: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Canada_%28Pantone%29.svg",
+    Continent: "North America"
+},
+{
+    Name: "Brazil",
+    Image: "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/640px-Flag_of_Brazil.svg.png",
+    Continent: "South America"
+},
+{
+    Name: "South Africa",
+    Image: "https://upload.wikimedia.org/wikipedia/commons/a/af/Flag_of_South_Africa.svg",
+    Continent: "Africa"
+},
+{
+    Name: "Japan",
+    Image: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/1200px-Flag_of_Japan.svg.png",
+    Continent: "Asia"
+},
+
+]
