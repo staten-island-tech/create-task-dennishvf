a413aeff91b32171c03010701e21bf8e9e779a49
dennishvf <dennisk38@nycstudents.net>
2024-01-17 08:56:28 -0500

    rgsaegw

--- javascript/dom.js
+++ javascript/dom.js
@@ -1,1 +1,5 @@
-D
+const Domselectors = {
+    Form: document.getElementById("form"),
+    EnterButton: document.getElementById("enter-button"),
+    SearchBar: document.getElementById("search-bar"),
+}
