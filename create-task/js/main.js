02dd5dc024950bf2bd721a8d3fe6b64ab8f08df7
dennishvf <dennisk38@nycstudents.net>
2024-01-19 09:23:20 -0500

    ukfnf

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -2,16 +2,14 @@ import {Domselectors} from './dom';
 import {Countries} from './countries';
 import "../css/style.css";
 const random = Math.floor(Math.random() * Countries.length);
-console.log(random, Countries[random]);
+const rc = (Countries[random])
 //Initial card
-function iinsert(arr){
-    arr[random] ((Country) => {
+function iinsert(country){
          document.getElementById("flex-container").insertAdjacentHTML("afterbegin",
      `<div class="card">
-     <h1 class="card-title">${arr[random].Name}</h1>
-     <img src="${Country.Image}" alt="" class="card-image">
+     <h1 class="card-title">${country.Name}</h1>
+     <img src="${country.Image}" alt="" class="card-image">
      </div>`)
-     });
-     
- }
- iinsert(Countries)
+     ;
+    }
+ iinsert(rc)
