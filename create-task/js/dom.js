39505914aa35324b0e2ac400c81a57c6eeb398ee
dennishvf <dennisk38@nycstudents.net>
2024-01-19 08:44:09 -0500

    jgkj,hgl.

--- create-task/js/dom.js
+++ create-task/js/dom.js
@@ -2,5 +2,6 @@ const Domselectors = {
     Form: document.getElementById("form"),
     EnterButton: document.getElementById("enter-button"),
     SearchBar: document.getElementById("search-bar"),
+    Card: document.querySelector(".card"),
 }
 export{Domselectors}
